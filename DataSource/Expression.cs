0d6a410b49c2eb278a4b77b35da143673ecc73d7
Progoktato <oktato.ber@gmail.com>
2023-05-26 23:56:34 +0200

    Átnézve a diákoké

---
+++ DataSource/Expression.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConOperatorok.DataSource
+{
+    public class Expression
+    {
+        static string[] UseableOperations = { "+", "-", "*", "/", "mod", "div" };
+
+        int operandLeft;
+        int operandRight;
+        string operation;
+
+        public Expression(string strLine)
+        {
+            string[] fields = strLine.Split();
+            if (fields.Length != 3)
+            {
+                throw new InvalidOperationException("Túl sok, vagy túl kevés tag a sorban!");
+            }
+            try
+            {
+                this.operandLeft = int.Parse(fields[0]);
+                this.operandRight = int.Parse(fields[2]);
+            }
+            catch (Exception)
+            {
+
+                throw new ArgumentException("Az egyik operandus nem egész szám formátumú!");
+            }
+            fields[1] = fields[1].ToLower();
+            this.operation = fields[1].ToLower();
+        }
+
+        public Expression(int operandLeft, int operandRight, string operation)
+        {
+            this.operandLeft = operandLeft;
+            this.operandRight = operandRight;
+            this.operation = operation;
+        }
+
+        public double OperandLeft { get => operandLeft; }
+        public double OperandRight { get => operandRight; }
+        public string Operation { get => this.operation; }
+        public bool IsValidOperation { get => UseableOperations.Any(x => x == this.Operation); }
+
+        public string Result
+        {
+            get
+            {
+                double? localResult = null;
+                try
+                {
+                    switch (operation)
+                    {
+                        case "+":
+                            localResult = operandLeft + operandRight;
+                            break;
+                        case "-":
+                            localResult = operandLeft - operandRight;
+                            break;
+                        case "*":
+                            localResult = operandLeft * operandRight;
+                            break;
+                        case "/":
+                            if (operandRight == 0)
+                            {
+                                throw new DivideByZeroException();
+                            }
+                            localResult = (double)operandLeft / operandRight;
+                            break;
+                        case "mod":
+                            localResult = operandLeft % operandRight;
+                            break;
+                        case "div":
+                            localResult = operandLeft / operandRight;
+                            break;
+                    }
+                    return $"{operandLeft} {operation} {operandRight} = " +
+                        $"{(localResult == null ? "Hibás operátor!" : localResult)}";
+                }
+                catch (Exception)
+                {
+                    return $"{operandLeft} {operation} {operandRight} = Egyéb hiba!";
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
-﻿namespace ConOperatorok
+﻿using ConOperatorok.DataSource;
+
+namespace ConOperatorok
 {
     internal class Program
     {
+        static List<Expression> expressions = new List<Expression>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            expressions = LoadExpressionsFromCSV("DataSource\\kifejezesek.txt");
+
+            Console.WriteLine($"2. feladat: Kifejezések száma: {expressions.Count}");
+
+            int resultCount = expressions.Count(x => x.Operation == "mod");
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {resultCount}");
+
+            bool resultAny = expressions.Any(x => x.OperandLeft % 10 == 0 && x.OperandRight % 10 == 0);
+            Console.WriteLine($"4. feladat: {(resultAny ? "Van" : "Nincs")} ilyen kfejezés");
+
+            Console.WriteLine("5. feladat: Statisztika");
+            expressions.Where(x => x.IsValidOperation)
+                       .GroupBy(x => x.Operation)
+                       .ToList()
+                       .ForEach(x => Console.WriteLine($"\t{x.Key} -> {x.Count()} db"));
+
+            string stringFromConsole = "";
+            do
+            {
+                Console.Write($"7. feladat: Kérek egy kifejezést (pl.: 1 + 1):");
+                stringFromConsole = Console.ReadLine();
+                if (stringFromConsole.ToLower() != "vége")
+                {
+                    Console.WriteLine($"\t{new Expression(stringFromConsole).Result}");
+                }
+            } while (stringFromConsole.ToLower() != "vége");
+
+            Console.WriteLine("8. feladat: eredmenyek.txt");
+            File.WriteAllLines("eredmenyek.txt",expressions.Select(ob=>ob.Result));
+        }
+
+        private static List<Expression> LoadExpressionsFromCSV(string fileName)
+        {
+            return File.ReadAllLines(fileName).ToList().Select(csvLine => new Expression(csvLine)).ToList();
         }
     }
 }
